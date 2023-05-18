8c4919dc91cd5d62eb24f660d5a09b243686da4e
wareya <wareya@gmail.com>
2023-05-18 10:39:40 -0400

    port to godot 4, gltf export

--- SewTest.gd
+++ SewTest.gd
@@ -1,7 +1,7 @@
-extends Spatial
+extends Node3D
 class_name SewTest
 
-static func build(verts_bottom, verts_top):
+static func build(vertices : PackedVector3Array, verts_bottom, verts_top):
     var closest_top = 0
     var closest_dist = (verts_bottom[0] - verts_top[0]).length()
     for i in range(1, verts_top.size()):
@@ -9,8 +9,6 @@ static func build(verts_bottom, verts_top):
         if dist < closest_dist:
             closest_dist = dist
             closest_top = i
-    
-    var vertices = PoolVector3Array()
     
     var looped_bottom = false
     var looped_top = false
@@ -46,30 +44,7 @@ static func build(verts_bottom, verts_top):
             
             if front_bottom == 0:
                 looped_bottom = true
-        
     
-    var arrays = []
-    arrays.resize(Mesh.ARRAY_MAX)
-    arrays[Mesh.ARRAY_VERTEX] = vertices
-    var mesh = ArrayMesh.new()
-    mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-    
-    var meshinstance = MeshInstance.new()
-    
-    meshinstance.mesh = mesh
-    
-    var builder = SurfaceTool.new()
-    builder.create_from(mesh, 0)
-    builder.generate_normals()
-    builder.commit(meshinstance.mesh)
-    
-    var mat = SpatialMaterial.new()
-    mat.albedo_color = Color(0.3, 0.2, 0.1, 1.0)
-    mat.params_cull_mode = SpatialMaterial.CULL_DISABLED
-    mat.params_diffuse_mode = SpatialMaterial.DIFFUSE_LAMBERT
-    meshinstance.material_override = mat
-    
-    return meshinstance
 
 func _ready():
     var verts_bottom = []
@@ -83,12 +58,12 @@ func _ready():
         var rad = i/8.0 * PI * 2.0
         verts_top.push_back(Vector3(sin(rad), 1, cos(rad)))
     
-    add_child(build(verts_bottom, verts_top))
+    #add_child(build(verts_bottom, verts_top))
 
 var sensitivity = 0.22
 func _input(_event):
     if _event is InputEventMouseMotion:
         var event : InputEventMouseMotion = _event
-        if event.button_mask & BUTTON_MASK_MIDDLE:
-            $Spatial.rotation_degrees.x -= event.relative.y * sensitivity
-            $Spatial.rotation_degrees.y -= event.relative.x * sensitivity
+        if event.button_mask & MOUSE_BUTTON_MASK_MIDDLE:
+            $Node3D.rotation_degrees.x -= event.relative.y * sensitivity
+            $Node3D.rotation_degrees.y -= event.relative.x * sensitivity
--- Spatial.gd
+++ Spatial.gd
@@ -1,7 +1,6 @@
-extends Spatial
+extends Node3D
 
-
-class TreeBit extends Reference:
+class TreeBit extends RefCounted:
     # storage
     var position = Vector3()
     var size = 1.0
@@ -24,11 +23,12 @@ class TreeBit extends Reference:
     var juice_gain_rate = 0.2
     
     func add_children():
+        var world = Engine.get_main_loop().current_scene.get_tree().get_nodes_in_group("World")[0]
         var heading : Vector3 = Vector3.UP
         if parent:
             heading = (position - parent.position).normalized()
             if heading.y < 0.0 or size > 0.3:
-                heading = heading.linear_interpolate(Vector3.UP, 0.2).normalized()
+                heading = heading.lerp(Vector3.UP, 0.2).normalized()
         
         end_ring = []
         var vert_count = int(size*8) + 1
@@ -39,9 +39,9 @@ class TreeBit extends Reference:
             var rad = i/float(vert_count) * PI * 2.0
             #var v = Vector3(sin(rad), 0, cos(rad)) * size
             var v = Vector3(sin(rad), cos(rad), 0) * size
-            var t : Transform = Transform.IDENTITY
+            var t : Transform3D = Transform3D.IDENTITY
             t = t.looking_at(heading, Vector3.UP + Vector3(0.0001, 0.00008981, -0.000014389))
-            v = t.basis.xform(v)
+            v = t.basis * (v)
             end_ring.push_back(v + position)
         
         juice += juice_gain_rate
@@ -103,128 +103,184 @@ var vec2_quad = [
     Vector2(1, 1),
 ]
 
-func add_children(bit : TreeBit, parent : Spatial):
-    #var geometry = CylinderMesh.new()
-    #var mat = SpatialMaterial.new()
-    #mat.albedo_color = Color.brown
-    #geometry.material = mat
-    #geometry.top_radius = bit.size * 0.5
-    #geometry.bottom_radius = bit.size * 0.5
-    #if bit.parent:
-    #    geometry.bottom_radius = bit.parent.size * 0.5
-    #geometry.height = bit.size
-    #if bit.parent:
-    #    geometry.height = (bit.position - bit.parent.position).length()
-    #
-    #var meshinstance = MeshInstance.new()
-    #meshinstance.mesh = geometry
-    #parent.add_child(meshinstance)
-    #meshinstance.global_translation = bit.position
-    #meshinstance.look_at(parent.global_translation + Vector3(0.0, -0.0001, 0.0), Vector3.UP + Vector3(0.00001, #-0.0001, 0.001))
-    #meshinstance.rotate_object_local(Vector3.RIGHT, PI*0.5)
-    #
-    #if bit.parent:
-    #    #meshinstance.global_translation = (bit.position - bit.parent.position).length()
-    #    meshinstance.translate_object_local(Vector3(0.0, geometry.height * -0.5, 0.0))
-    #else:
-    #    meshinstance.translate_object_local(Vector3(0.0, geometry.height * 0.5, 0.0))
-    #
-    #for child in bit.children:
-    #    add_children(child, meshinstance)
-    
+func build_leaf_mat():
+    var mat = StandardMaterial3D.new()
+    mat.albedo_texture = preload("res://art/just leaves.png")
+    mat.params_diffuse_mode = StandardMaterial3D.DIFFUSE_LAMBERT
+    mat.params_use_alpha_scissor = true
+    return mat
+
+@onready var leaf_mat = build_leaf_mat()
+
+func build_bark_mat():
+    var mat = StandardMaterial3D.new()
+    mat.albedo_color = Color(0.3, 0.2, 0.1, 1.0)
+    mat.params_cull_mode = StandardMaterial3D.CULL_DISABLED
+    mat.params_diffuse_mode = StandardMaterial3D.DIFFUSE_LAMBERT
+    return mat
+
+@onready var bark_mat = build_bark_mat()
+
+            
+var bark_vertices = PackedVector3Array()
+
+var leaf_verts   = PackedVector3Array()
+var leaf_uvs     = PackedVector2Array()
+var leaf_normals = PackedVector3Array()
+
+func add_children(bit : TreeBit, parent : Node3D) -> void:
     if bit.parent:
-        var geo = SewTest.build(bit.parent.end_ring, bit.end_ring)
-        parent.add_child(geo)
+        SewTest.build(bark_vertices, bit.parent.end_ring, bit.end_ring)
         for child in bit.children:
             add_children(child, geo)
         
         if bit.size < 0.4:
             for leaf in leaves:
                 if (bit.position - leaf).length() < 1.5:
-                    return geo
+                    return
             
             var size = lerp(6.0, 12.0, bit.size * 2.0)
             
-            var xform : Transform = Transform.IDENTITY
+            var xform : Transform3D = Transform3D.IDENTITY
             xform = xform.rotated(Vector3.UP, randf() * PI * 2.0)
             xform = xform.rotated(Vector3.RIGHT, randf() * PI * 2.0)
             xform = xform.rotated(Vector3.BACK, randf() * PI * 2.0)
             #xform = xform.translated()
             
-            var verts   = PoolVector3Array()
-            var uvs     = PoolVector2Array()
-            var normals = PoolVector3Array()
-            
             for i in [0, 1, 2, 2, 1, 3, 0, 2, 1, 1, 2, 3]:
-                var vert = xform.xform(vec3_quad[i] * size / 2.0)
-                verts  .push_back(vert)
-                uvs    .push_back(vec2_quad[i])
+                var vert = xform * (vec3_quad[i] * size / 2.0)
+                leaf_verts.push_back(vert + bit.position)
+                leaf_uvs.push_back(vec2_quad[i])
                 
                 var normal_a = (vert + bit.position).normalized()
                 var normal_b = (vert + bit.position - Vector3(0, 8, 0)).normalized()
                 var dist_to_dummy = (vert + bit.position - Vector3(0, 8, 0)).length()
                 var rank = clamp(dist_to_dummy / 8.0, 0.0, 1.0)
-                var normal = normal_b.linear_interpolate(normal_a, rank).normalized()
+                var normal = normal_b.lerp(normal_a, rank).normalized()
                 #var normal = normal_a
                 
-                normals.push_back(normal)
-            
-            var arrays = []
-            arrays.resize(Mesh.ARRAY_MAX)
-            arrays[Mesh.ARRAY_VERTEX] = verts
-            arrays[Mesh.ARRAY_TEX_UV] = uvs
-            arrays[Mesh.ARRAY_NORMAL] = normals
-
-            var mesh = ArrayMesh.new()
-            mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-            
-            var card = MeshInstance.new()
-            #var mesh = QuadMesh.new()
-            #mesh.size = Vector2.ONE * size
-            card.mesh = mesh
-            var mat = SpatialMaterial.new()
-            mat.albedo_texture = preload("res://art/just leaves.png")
-            
-            mat.params_diffuse_mode = SpatialMaterial.DIFFUSE_LAMBERT
-            mat.params_use_alpha_scissor = true
-            #mat.params_billboard_mode = SpatialMaterial.BILLBOARD_ENABLED
-            #mat.params_cull_mode = SpatialMaterial.CULL_DISABLED
-            card.material_override = mat
-            
-            geo.add_child(card)
-            card.global_translation = bit.position
+                leaf_normals.push_back(normal)
             
             leaves.push_back(bit.position)
-        return geo
+        return
     else:
-        var start_ring = []
-        var vert_count = int(bit.size*8) + 1
-        for i in range(vert_count):
-            var rad = i/float(vert_count) * PI * 2.0
-            start_ring.push_back(Vector3(sin(rad), 0, cos(rad)) * bit.size)
-        
-        var geo = SewTest.build(start_ring, bit.end_ring)
-        parent.add_child(geo)
         for child in bit.children:
             add_children(child, geo)
-        return geo
+        return
 
 var leaves = []
 var geo = null
+var current_seed = hash(str(Time.get_unix_time_from_system()))
 func new_tree():
     if geo:
         geo.queue_free()
         remove_child(geo)
+    
+    bark_vertices.clear()
+    leaf_verts.clear()
+    leaf_uvs.clear()
+    leaf_normals.clear()
+    
     leaves = []
     
-    randomize()
-    #seed(27452)
+    current_seed = hash(str(Time.get_unix_time_from_system()))
+    print("seed: ", current_seed)
+    seed(current_seed)
+    
     var root = TreeBit.new()
     root.add_children()
-    geo = add_children(root, self)
+    add_children(root, self)
+    
+    var mesh = ArrayMesh.new()
+    
+    # add wood
+    
+    var bark_arrays = []
+    bark_arrays.resize(Mesh.ARRAY_MAX)
+    bark_arrays[Mesh.ARRAY_VERTEX] = bark_vertices
+    mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, bark_arrays)
+    var bark_surface = mesh.get_surface_count() - 1
+    mesh.surface_set_material(bark_surface, bark_mat)
+    
+    var tool = SurfaceTool.new()
+    tool.create_from(mesh, bark_surface)
+    tool.generate_normals()
+    mesh.clear_surfaces()
+    tool.commit(mesh)
+    
+    # add leaves
+    
+    var leaf_arrays = []
+    leaf_arrays.resize(Mesh.ARRAY_MAX)
+    leaf_arrays[Mesh.ARRAY_VERTEX] = leaf_verts
+    leaf_arrays[Mesh.ARRAY_TEX_UV] = leaf_uvs
+    leaf_arrays[Mesh.ARRAY_NORMAL] = leaf_normals
+
+    mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, leaf_arrays)
+    var leaf_surface = mesh.get_surface_count() - 1
+    mesh.surface_set_material(leaf_surface, leaf_mat)
+    
+    # add to world
+    
+    geo = MeshInstance3D.new()
+    geo.mesh = mesh
+    add_child(geo)
+
+class MultiSignal extends RefCounted:
+    signal first(int)
+    signal all(int)
+    var count : int = 0
+    var total : int = 0
+    func connectify(sig : Signal, which : int):
+        if count != 0:
+            # refuse new connections if at least one signal has already gone off
+            return
+        total += 1
+        
+        var r = await sig
+        
+        count += 1
+        if count == 1:
+            first.emit(which)
+        if count == total:
+            all.emit(which)
+
+func save_gltf():
+    var dialog = FileDialog.new()
+    dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+    dialog.access = FileDialog.ACCESS_FILESYSTEM
+    dialog.add_filter("*.glb", "GLTF File")
+    dialog.current_file = "tree.glb"
+    add_child(dialog)
+    dialog.popup_centered_ratio()
+    
+    var multi = MultiSignal.new()
+    multi.connectify(dialog.file_selected, 0)
+    multi.connectify(dialog.close_requested, 1)
+    multi.connectify(dialog.canceled, 2)
+    var which = await multi.first
+    
+    dialog.queue_free()
+    
+    print(which)
+    
+    if which == 0 or which == 3:
+        var fname = dialog.current_path
+        print(fname)
+        
+        var gltf = GLTFDocument.new()
+        var gltf_state = GLTFState.new()
+        gltf.append_from_scene(geo, gltf_state)
+        var buffer = gltf.generate_buffer(gltf_state)
+        
+        var f = FileAccess.open(fname, FileAccess.WRITE)
+        f.store_buffer(buffer)
+        f.close()
+    
 
 func _ready():
-    $Button.connect("pressed", self, "new_tree")
+    $Button.connect("pressed", Callable(self, "new_tree"))
+    $Button2.connect("pressed", Callable(self, "save_gltf"))
     new_tree()
 
 var sensitivity = 0.22
@@ -232,19 +288,19 @@ func _input(_event):
     if _event is InputEventMouseButton:
         var event : InputEventMouseButton = _event
         if event.button_index == 4 and event.pressed:
-            $Spatial/Camera.translation.z *= 0.8
+            $Node3D/Camera3D.position.z *= 0.8
         elif event.button_index == 5 and event.pressed:
-            $Spatial/Camera.translation.z /= 0.8
-        $Spatial/Camera.translation.z = clamp($Spatial/Camera.translation.z, 1, 64)
+            $Node3D/Camera3D.position.z /= 0.8
+        $Node3D/Camera3D.position.z = clamp($Node3D/Camera3D.position.z, 1, 64)
     
     if _event is InputEventMouseMotion:
         var event : InputEventMouseMotion = _event
-        if event.button_mask & BUTTON_MASK_MIDDLE:
-            if event.shift:
-                var scale = sensitivity * $Spatial/Camera.translation.z * 0.01
-                $Spatial.translate_object_local(Vector3.UP    * event.relative.y * scale)
-                $Spatial.translate_object_local(Vector3.RIGHT * -event.relative.x * scale)
+        if event.button_mask & MOUSE_BUTTON_MASK_MIDDLE:
+            if event.shift_pressed:
+                var scale = sensitivity * $Node3D/Camera3D.position.z * 0.01
+                $Node3D.translate_object_local(Vector3.UP    * event.relative.y * scale)
+                $Node3D.translate_object_local(Vector3.RIGHT * -event.relative.x * scale)
             else:
-                $Spatial.rotation_degrees.x -= event.relative.y * sensitivity
-                $Spatial.rotation_degrees.y -= event.relative.x * sensitivity
+                $Node3D.rotation_degrees.x -= event.relative.y * sensitivity
+                $Node3D.rotation_degrees.y -= event.relative.x * sensitivity
     
